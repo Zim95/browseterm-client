58caf00a8d4883043d11bba1f6b34c55d974e957
Zim95 <shresthanamah@gmail.com>
2023-12-05 13:58:26 +0530

    Getting createContainer to work. Part 3. Integrated with actual container api. Need to assign IP address upon starting.

--- src/components/containers/containers-list/container-list-buttons/ContainerListButtons.js
+++ src/components/containers/containers-list/container-list-buttons/ContainerListButtons.js
@@ -1,23 +1,61 @@
 import React, {useState} from 'react';
 import "./ContainerListButtons.css";
+import config from '../../../../config';
 
 function ContainerListButtons({removeContainer, containerValue}) {
     const [containerState, setContainerState] = useState("stopped");
 
-    const stopContainer = () => {
+    const baseURL = config.DevAPIRequestsConfig.containerAPI.urls.baseURL;
+    const headers = config.DevAPIRequestsConfig.containerAPI.headers;
+    const body = JSON.stringify(
+        {
+            container_ids: containerValue.full_ids,
+            container_network: containerValue.network,
+        }
+    );
+    const fetchJson = {
+        method: 'POST',
+        headers: headers,
+        body: body
+    }
+
+    const makeRequest = async (offset, success) => {
+        const url = baseURL + offset;
+        const response = await fetch(url, fetchJson);
+        if (response.status == 200) {
+            success();
+        } else {
+            const error_message = await response.text();
+            console.error(error_message);
+        }
+    }
+
+    const stopContainer = async () => {
         console.log("Stopping Container...");
-        setContainerState("stopped");
+        const success = () => {
+            setContainerState("stopped");
+        };
+        const offset = config.DevAPIRequestsConfig.containerAPI.urls.stopContainerOffset;
+        makeRequest(offset, success);
     };
 
     const startContainer = () => {
         console.log("Starting Container...");
-        setContainerState("started");
+        const success = () => {
+            setContainerState("started");
+        };
+        const offset = config.DevAPIRequestsConfig.containerAPI.urls.startContainerOffset;
+        makeRequest(offset, success);
     };
 
     const deleteContainer = () => {
         console.log("Deleting Container...");
-        removeContainer(containerValue);
-        setContainerState("deleted");
+        const success = () => {
+            removeContainer(containerValue);
+            setContainerState("deleted");
+        };
+        const offset = config.DevAPIRequestsConfig.containerAPI.urls.deleteContainerOffset;
+        makeRequest(offset, success);
     };
 
     const saveContainer = () => {
