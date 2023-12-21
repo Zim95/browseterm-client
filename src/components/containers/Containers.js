9388b6cc280f5aab65212b1d765887e79abec212
Zim95 <shresthanamah@gmail.com>
2023-12-21 15:28:50 +0530

    Adding socketSSHContainer setup - 1. Adding create and delete workflow

--- src/components/containers/Containers.js
+++ src/components/containers/Containers.js
@@ -2,10 +2,96 @@ import React, {useEffect, useState} from 'react';
 import ContainersForm from './containers-form/ContainersForm';
 import ContainersList from './containers-list/ContainersList';
 import "./Containers.css";
+import { config } from '@fortawesome/fontawesome-svg-core';
 
 function Containers() {
   const [containerData, setContainerData] = useState({});
-  const [sshSocketCount, setSshSocketCount] = useState(0);
+  const [socketSSHContainer, setSocketSSHContainer] = useState(null);
+
+  const startSocketSSH = async (container) => {
+    try {
+      const startHeaders = config.containerAPI.headers;
+      const startUrl = config.containerAPI.urs.baseURL + config.containerAPI.urls.startContainerOffset;
+      const startData = JSON.stringify(
+        {
+            "container_ids": [container.container_id],
+            "container_name": container.container_name,
+            "container_network": container.container_network
+        }
+      );
+      const response = await fetch(
+          startUrl,
+          {
+              method: "POST",
+              headers: startHeaders,
+              body: startData,
+          }
+      );
+      if (response.status !== 200) {
+        const error_message = await response.text();
+        throw new Error(`HTTP Error! ${error_message }`);
+      }
+      const data = await response.json();
+      setSocketSSHContainer(data.response[0]);
+    } catch (error) {
+      throw new Error("Error while starting container", error);
+    }
+  };
+
+  const createSocketSSH = async () => {
+    try {
+        const userName = "zim95";
+        const containerName = userName + "_socket_ssh_container";
+        const containerNetwork = userName + "_network";
+        const postData = JSON.stringify({
+            "image_name": "zim95/socket-ssh:latest",
+            "container_name": containerName,
+            "container_network": containerNetwork,
+            "publish_information": {
+              "8000/tcp": 8000
+            }
+        });
+        const postHeaders = config.containerAPI.headers;
+        const postUrl = config.containerAPI.urls.baseURL + config.containerAPI.urls.createContainerOffset;
+        const response = await fetch(
+            postUrl,
+            {
+                method: "POST",
+                headers: postHeaders,
+                body: postData,
+            }
+        );
+        if (response.status !== 200) {
+            const error_message = await response.text();
+            throw new Error(`HTTP Error! ${error_message }`);
+        }
+        const data = await response.json();
+        await startSocketSSH(data.response[0]);
+    } catch (error) {
+        throw new Error("Error create socket ssh", error);
+    }
+  };
+
+  const removeSocketSSH = async () => {
+    try {
+      console.log("Removing", socketSSHContainer);
+    } catch(error) {
+      throw new Error(error)
+    }
+  };
+
+  useEffect(async () => {
+    try {
+      await createSocketSSH();
+      window.addEventListener('beforeunload', unloadContainer);
+      return () => {
+          window.removeEventListener('beforeunload', unloadContainer);
+      };
+    } catch (error) {
+      console.error(error);
+    }
+  }, [socketSSHContainer]);
+
 
   const addContainer = (containerResponse) => {
     let containerDataMap = {};
