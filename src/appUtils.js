80fcfdc9dbea3760e7c196122284efa913a24b9e
Zim95 <shresthanamah@gmail.com>
2023-12-30 15:36:41 +0530

    Code cleanup 1 - added appUtils.js and ContainerUtils.js

---
+++ src/appUtils.js
@@ -0,0 +1,78 @@
+/*
+    This is the App Utils file. This file has common utility functions and classes
+    that can be used throughout the app files.
+
+    Author: Namah Shrestha
+*/
+
+
+export class RequestMaker {
+    /*
+        Request Maker for making API Requests.
+
+        Author: Namah Shrestha
+    */
+    constructor(method, url, body=null, headers=null) {
+        /*
+            Initialize parameters.
+            :params:
+                method: String: 'GET/POST/PUT/DELETE...'.
+                url: String: URL to make the request to.
+                body: JSON | null: Body of the request.
+                headers: JSON | null: Headers of the request.
+
+            Author: Namah Shrestha
+        */
+        this.method = method;
+        this.url = url;
+        this.body = body;
+        this.headers = headers;
+    }
+
+    makeRequest = async() => {
+        /*
+            This is the make request method which makes the request based on parameters.
+
+            Author: Namah Shrestha
+        */
+        let requestOptions = {
+            method: this.method
+        }
+        if(this.headers !== null) {
+            requestOptions.headers = this.headers;
+        }
+        if (this.body !== null) {
+            requestOptions.body = JSON.stringify(this.body);
+        }
+        const response = await fetch(this.url, requestOptions);
+        return response;
+    };
+
+    parseResponse = async(response) => {
+        /*
+            Parses the successful response and returns data.
+        */
+        const data = await response.json();
+        return data.response;
+    };
+
+    callOnce = async() => {
+        const response = this.makeRequest();
+        if (response.status !== 200) {
+            const error = await response.text();
+            throw new Error("Make Request Error", error);
+        }
+        const data = await this.parseResponse(response);
+        return data;
+    };
+
+    callUntilSuccess = async() => {
+        const response = this.makeRequest();
+        if (response.status !== 200) {
+            this.callUntilSuccess();
+        } else {
+            const data = await this.parseResponse(response);
+            return data;
+        } 
+    };
+}
--- src/components/containers/ContainerUtils.js
+++ src/components/containers/ContainerUtils.js
@@ -1,49 +1,31 @@
+/*
+    This is the ContainerUtils file. All utility methods that are common
+    within container operations are stored here.
+
+    Author: Namah Shrestha
+*/
+
+// module imports
 import config from "../../config";
-
-
-class MakeRequest {
-    makeRequest = async(method, url, body) => {
-        const headers = config.containerAPI.headers;
-        const payload = JSON.stringify(body);
-
-        const response = await fetch(
-            url,
-            {
-                method: method,
-                headers: headers,
-                body: payload,
-            }
-        );
-        return response;
-    };
-
-    parseResponse = async(response) => {
-        if(response.status !== 200) {
-            const error = await response.text();
-            throw new Error("Make Request Error", error);
-        }
-
-        const data = await response.json();
-        return data;
-    };
-
-    callOnce = async(method, url, body) => {
-
-    };
-
-    callUntilSuccess = async(method, url, body) => {
-        
-    };
-}
+import { RequestMaker } from "../../appUtils";
 
 
 class ContainerManager {
     /*
-        Create, Start, Stop, Delete Containers.
+        Create, Start, Stop, Delete Containers by making requests to the Backend API.
 
         Author: Namah Shrestha
     */
     constructor() {
+        /*
+            Initialize the parameters as per container API.
+            - Initialize: create, start, stop, delete, beacon URLs.
+            - Initialize: method.
+            - Initialize: headers. Load token from localStorage and put it in headers.
+        
+            Author: Namah Shrestha
+        */
+
         // urls
         this.baseUrl = config.containerAPI.urls.baseURL;
         this.createUrl = this.baseUrl + config.containerAPI.urls.createContainerOffset;
@@ -55,12 +37,56 @@ class ContainerManager {
         // methods
         this.method = "POST";
 
-        // request maker
-        this.requestMaker = new MakeRequest();
+        //headers
+        this.headers = config.containerAPI.headers;
+        /*
+            TODO: Get the token from localStorage and put it in headers.
+        */
     }
 
-    createContainer = async() => {
+    createContainer = async(
+        imageName,
+        containerName,
+        containerNetwork,
+        publishInformation=null,
+        environment=null
+    ) => {
+        /*
+            The createContainer method. Makes a POST request to rest container deployment.
+            :params:
+                imageName: String: Name of the image to use.
+                containerName: String: Name of the container being created.
+                containerNetwork: String: Name of the network in which the container
+                                          will be created.
+                publishInformation: JSON | null: Port mapping for the container.
+                                                 Eg: {'8000/tcp': 8000}.
+                environment: JSON | null: Environment Variables.
+                                          Eg: {'SSH_PASSWORD': '1234'}
 
+            Author: Namah Shrestha
+        */
+        let createContainerBody = {
+            "image_name": imageName,
+            "container_name": containerName,
+            "container_network": containerNetwork
+        };
+        if(publishInformation !==  null) {
+            createContainerBody["publish_information"] = publishInformation;
+        }
+        if(environment !== null) {
+            createContainerBody["environment"] = environment;
+        }
+
+        const createRequestMaker = new RequestMaker(
+            this.method, this.createUrl, body=createContainerBody, headers=this.headers
+        );
+
+        try{
+            const response = createRequestMaker.callOnce();
+            return response;
+        } catch(error) {
+            throw new Error("Create Container Error", error);
+        }
     };
 }
 
