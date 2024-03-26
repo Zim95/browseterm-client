937c98e9af8f87681a663e1dcbb76650781e2d34
Namah Shrestha <namah.shrestha@tgs.com>
2024-03-27 04:12:25 +0530

    Modified protected component. Added switch case and useEffect.

--- src/components/Protected.js
+++ src/components/Protected.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { BaseAuthService } from "../lib/authUtils";
 
 
 function Protected({children}) {
-    if (BaseAuthService.isLoggedIn()) {
-        return children;
+    const [accessible, setAccessible] = useState(null);
+
+    useEffect(() => {
+        setAccessible(BaseAuthService.isLoggedIn());
+    }, []);
+
+    switch(accessible) {
+        case true:
+            return children;
+        case false:
+            return <Navigate to="/signin"/>
+        case null:
+            return <div className="expanded-centered"> Loading... </div>
     }
-    return <Navigate to="/signin"/>
 }
 
 export default Protected;
